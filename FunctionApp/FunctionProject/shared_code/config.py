9ebbb66b94a177a01f7c66eb70995170285da00f
Katia Gil Guzman <kagilguz@microsoft.com>
2019-05-24 18:02:46 +0200

    added Azure Python Function

--- FunctionApp/FunctionProject/shared_code/config.py
+++ FunctionApp/FunctionProject/shared_code/config.py
@@ -1,5 +1,5 @@
 blobConfig = {
-    'ACCOUNTNAME': 'katiastorage',
-    'KEY': "TQejr8UxBlu4Y4h/vTuJU6RrXHFEdImTCn6JmhcFyIToztbdAvvnX5t6Xg68k/s3KR0oRCEWac/lzO3NfPoKBA==",
-    'CONTAINER': 'docs'
+    'ACCOUNTNAME': 'YOUR_ACCOUNT_NAME',
+    'KEY': "YOUR_KEY",
+    'CONTAINER': 'CONTAINER_NAME'
 }
